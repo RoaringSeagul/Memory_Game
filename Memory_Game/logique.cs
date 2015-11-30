dd2f7069a4b3e9de308723b346489ed9a01488de
roaringseagul <jokoro@cabanot.com>
2015-11-30 13:42:51 -0500

    Reparation mergei

--- Memory_Game/logique.cs
+++ Memory_Game/logique.cs
@@ -14,24 +14,20 @@ namespace Memory_Game
 
         public logique(int size)
         {
+            lstCoordUsed = new CoordUsed[size, size];
+            lstImage = new ImageCarte[size, size];
+
             for (int x = 0; x < size; x++)
                 for (int y = 0; y < size; y++)
                     lstCoordUsed[x, y] = new CoordUsed();
 
-            switch (size)
-            {
-                case 8:
-                    NewGame8x8();
-                    break;
-                default:
-                    break;
-            }
+            NewGame(size);
         }
 
-        void NewGame8x8()
+        void NewGame(int size)
         {
-            int rndX = _rnd.Next(0, 8);
-            int rndY = _rnd.Next(0, 8);
+            int rndX = _rnd.Next(0, size);
+            int rndY = _rnd.Next(0, size);
             if (!lstCoordUsed[rndX, rndY].IsUsed)
             {
                 lstImage[rndX, rndY] = new ImageCarte();
@@ -39,13 +35,15 @@ namespace Memory_Game
                 lstCoordUsed[rndX, rndY].IsUsed = true;
             }
 
-            lstImage = new ImageCarte[8, 8];
-            for (int x = 0; x < 8; x++)
+            for (int x = 0; x < size; x++)
             {
-                for (int y = 0; y < 8; y++)
+                for (int y = 0; y < size; y++)
                 {
-                    lstImage[x, y] = new ImageCarte();
-                    lstImage[x, y].imageName = "sapote";
+                    if (!lstCoordUsed[x, y].IsUsed)
+                    {
+                        lstImage[x, y] = new ImageCarte();
+                        lstImage[x, y].imageName = "sapote";
+                    }
                 }
             }
         }
